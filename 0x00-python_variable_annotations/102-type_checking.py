56a20be7ce88b2fd8c765407acfc2874220bcd26
eMode18 <ewagandi@gmail.com>
2024-06-07 09:05:29 +0000

    102-type_checking.py correction

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,15 +1,33 @@
 #!/usr/bin/env python3
-"""task advanced 102"""
+"""
+102-type_checking.py: A simple module that defines the `zoom_array` function.
 
-from typing import Tuple
+This module provides a function `zoom_array` that takes an input tuple (`lst`)
+and an optional integer factor. It returns a new tuple where each
+element from the input tuple is repeated `factor` times.
+"""
+
+from typing import Tuple, Any
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
+    """
+    Returns a new tuple where each element from the input tuple (`lst`) is
+    repeated `factor` times.
+
+    Args:
+        lst (Tuple): Input tuple.
+        factor (int, optional): Repetition factor. Defaults to 2.
+
+    Returns:
+        Tuple: New tuple with repeated elements.
+    """
     zoomed_in: Tuple = tuple(
         item for item in lst
         for i in range(factor)
     )
     return zoomed_in
+
 
 array = (12, 72, 91)
 
